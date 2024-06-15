12abe41911116b4c74a74960518a4573b159990d
CHAITSNIPER <136554982+CHAITSNIPER@users.noreply.github.com>
2024-06-15 22:25:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,7 @@ import TextEditor from "./Components/TextEditor";
 import './all.css';
 
 function Home() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const id = uuidV4();
-    navigate(`/docs/${id}`);
-  }, [navigate]);
-
-  return null;
-}
+  
 
 function App() {
   return (
